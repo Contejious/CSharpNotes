cce69d88840d67c26c6691d7a7adc4495bf1d127
Jace Bennett <jace.bennett@gmail.com>
2013-11-19 22:08:22 -0500

    Added Echo example

---
+++ Echo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo {
+  class Program {
+    static void Main(string[] args)
+    {
+      var message = args[0];
+      var count = Convert.ToInt32(args[1]);
+
+      while (count > 0)
+      {
+        Console.WriteLine(message);
+        count--;
+      }
+    }
+  }
+}
