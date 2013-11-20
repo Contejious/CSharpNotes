860ffed495bba6a9bd984ab49882e8c6ae541c90
Jace Bennett <jace.bennett@gmail.com>
2013-11-19 22:36:04 -0500

    Guessing Game with methods.

--- GuessingGame/GuessingGame.cs
+++ GuessingGame/GuessingGame.cs
@@ -6,20 +6,21 @@ using System.Threading.Tasks;
 
 namespace GuessingGame {
   public class GuessingGame {
-    public void Play()
-    {
-      var randomNumberGenerator = new Random();
-      var correctAnswer = randomNumberGenerator.Next(1, 1000);
-
+    public void Play() {
+      var correctAnswer = PickANumber();
       Console.WriteLine("I have picked a number between 1 and 1000, try to guess it.");
+      var victory = DoGuesses(correctAnswer);
 
+      ShowResults(victory, correctAnswer);
+    }
+
+    public bool DoGuesses(int correctAnswer) {
       var victory = false;
 
       var guessesRemaining = 10;
 
       while (guessesRemaining > 0) {
-        Console.Write("Enter your guess:");
-        var guess = Convert.ToInt32(Console.ReadLine());
+        var guess = GetNextGuess();
 
         if (guess == correctAnswer) {
           victory = true;
@@ -35,6 +36,20 @@ namespace GuessingGame {
         guessesRemaining--;
       }
 
+      return victory;
+    }
+
+    public int PickANumber() {
+      var randomNumberGenerator = new Random();
+      return randomNumberGenerator.Next(1, 1000);
+    }
+
+    public int GetNextGuess() {
+      Console.Write("Enter your guess:");
+      return Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static void ShowResults(bool victory, int correctAnswer) {
       if (victory) {
         Console.WriteLine("Congrats, you guessed it!");
       }
