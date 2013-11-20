10fa980d77bbb91d51f09616c9ee28a8732cb95a
Jace Bennett <jace.bennett@gmail.com>
2013-11-19 22:33:04 -0500

    Game as a class

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,41 +6,10 @@ using System.Threading.Tasks;
 
 namespace GuessingGame {
   class Program {
-    static void Main(string[] args) {
-      var randomNumberGenerator = new Random();
-      var correctAnswer = randomNumberGenerator.Next(1, 1000);
-
-      Console.WriteLine("I have picked a number between 1 and 1000, try to guess it.");
-
-      var victory = false;
-
-      var guessesRemaining = 10;
-
-      while (guessesRemaining > 0) {
-        Console.Write("Enter your guess:");
-        var guess = Convert.ToInt32(Console.ReadLine());
-
-        if (guess == correctAnswer) {
-          victory = true;
-          break;
-        }
-        else if (guess < correctAnswer) {
-          Console.WriteLine("Higher.");
-        }
-        else if (guess > correctAnswer) {
-          Console.WriteLine("Lower.");
-        }
-
-        guessesRemaining--;
-      }
-
-      if (victory) {
-        Console.WriteLine("Congrats, you guessed it!");
-      }
-      else {
-        Console.WriteLine("Sorry, my number was " + correctAnswer + ". Better luck next time.");
-      }
-
+    static void Main(string[] args)
+    {
+      var game = new GuessingGame();
+      game.Play();
     }
   }
 }
